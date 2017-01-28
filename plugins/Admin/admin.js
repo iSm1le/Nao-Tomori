4de9825c91fe9c2ac7bd66cbcbb51507cd63c36b
iSm1le <sm1leua@ya.ru>
2017-01-28 11:23:16 +0200

    Added give/deny Access commands.

--- plugins/Admin/admin.js
+++ plugins/Admin/admin.js
@@ -2,6 +2,8 @@ var Permissions = require("../../permissions.json");
 
 exports.commands = [
 	"setUsername",
+	"giveAccess",
+	"denyAccess",
 	"log",
 	"uptime"
 ]
@@ -16,6 +18,80 @@ exports.setUsername = {
 			bot.user.setUsername(suffix);
 		} else {
 			msg.channel.sendMessage("You dont have permission to do this.");
+		}
+	}
+}
+
+exports.giveAccess = {
+	usage: "<user>",
+	description: "Grants a permission to user on server",
+	process: function(bot,msg,suffix){
+		try {
+			var arole = msg.guild.roles.find("name", Permissions.global.addOtherPermissionRole.slice(0, -1));
+			var role = msg.guild.roles.find("name", Permissions.global.addOtherPermissionRole);
+			if(msg.member.roles.has(role.id)) {
+				if(!msg.guild.member(bot.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")){
+					return msg.channel.sendMessage("I dont have permission (MANAGE_ROLES_OR_PERMISSIONS) to do this.");
+				}
+				if(msg.mentions.users.size === 0) {
+					return msg.channel.sendMessage("Please mention a user to grant a server permission.");
+				}
+				var auser = msg.guild.member(msg.mentions.users.first());
+				if(auser && arole)
+					{
+						if(!auser.roles.has(arole.id))
+							{
+								auser.addRole(arole);
+								msg.channel.sendMessage(`${auser}, you are permitted on server by ${msg.author}.`);
+							} else {
+								msg.channel.sendMessage(`${auser} already have permission to access server.`);
+							}
+					}
+			} else {
+				msg.channel.sendMessage("You dont have permission to do this.");
+			}
+		} catch(e) {
+			console.log(e);
+			msg.channel.sendMessage("I cant complete this task.");
+		}
+	}
+}
+
+exports.denyAccess = {
+	usage: "<user>",
+	description: "Deny a user permission on server",
+	process: function(bot,msg,suffix){
+		try {
+			var drole = msg.guild.roles.find("name", Permissions.global.addOtherPermissionRole.slice(0, -1));
+			var role = msg.guild.roles.find("name", Permissions.global.addOtherPermissionRole);
+			if(msg.member.roles.has(role.id)) {
+				if(!msg.guild.member(bot.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")){
+					return msg.channel.sendMessage("I dont have permission (MANAGE_ROLES_OR_PERMISSIONS) to do this.");
+				}
+				if(msg.mentions.users.size === 0) {
+					return msg.channel.sendMessage("Please mention a user to deny a server permission.");
+				}
+				var duser = msg.guild.member(msg.mentions.users.first());
+				if(!duser.roles.has(role.id)){
+					if(duser && drole)
+						{
+							if(duser.roles.has(drole.id))
+								{
+									duser.removeRole(drole);
+									msg.channel.sendMessage(`${duser}, you are denied on server by ${msg.author}.`);
+								} else {
+									msg.channel.sendMessage(`${duser} doesnt have permission at all.`);
+								}
+						}
+					} else {
+						return msg.channel.sendMessage(`I cant deauthorize ${duser} because he have a role ${Permissions.global.addOtherPermissionRole}`);
+					}
+			} else {
+				msg.channel.sendMessage("You dont have permission to do this.");
+			}
+		} catch(e) {
+			console.log(e);
+			msg.channel.sendMessage("I cant complete this task.");
 		}
 	}
 }
